aba82e9c74bd4c554cf44d8e470f49d69b071471
Nick Prühs <dev@npruehs.de>
2015-03-04 15:22:37 +0100

    ADDED: Events - Sender id.

--- Source/PinnedDownCore/PinnedDownCore/Event.h
+++ Source/PinnedDownCore/PinnedDownCore/Event.h
@@ -5,11 +5,16 @@
 #include "HashedString.h"
 #include "NetRole.h"
 
+#define INVALID_SENDER_ID -1
+
 namespace PinnedDownCore
 {
 	class Event
 	{
 	public:
+		// Id of the sending client if sent over the network, and INVALID_SENDER_ID otherwise.
+		int sender = INVALID_SENDER_ID;
+
 		virtual const HashedString & GetEventType() const = 0;
 
 		virtual std::string ToString() const
